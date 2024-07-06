ed066a5f6440e27af7cbf902ec12949107a6dff9
Ronney Arias <ronneyarias@gmail.com>
2024-07-06 12:36:49 -0500

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES 
+    (1, 'Mr. Chocolate', 23, 'M', 'Brown', 'Sweet', 1),
+    (2, 'Rowdy', 5, 'F', 'Red', 'Mean', 1),
+    (3, 'Tabitha', 45, 'F', 'Purple', 'Quiet', 1),
+    (4, 'Sergeant Brown', 63, 'M', 'White', 'Aggressive', 0),
+    (5, 'Melissa', 34, 'F', 'Pink', 'Calm', 1),
+    (6, 'Grinch', 789, 'M', 'Green', 'Mean', 1),
+    (7, 'Wendy', 20, 'F', 'Orange', 'Warm', 1),
+    (8, NULL, 567, 'F', 'Black', 'Varies', 1)
